0b98d9c40fb787660e5ea1fae98d0693cf3fb19e
Ing. Francisco <espinozacastellon@hotmail.com>
2019-09-30 15:40:41 -0600

    Vista Index

---
+++ NiscoutFBL2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NiscoutFBL2019.Startup))]
+namespace NiscoutFBL2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
