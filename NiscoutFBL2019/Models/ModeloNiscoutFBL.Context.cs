f5dde530c32c11627d4351276810ce2c74043b53
Ing. Francisco <Ing. Francisco@DIOS>
2019-12-09 13:51:10 -0600

    no se

--- NiscoutFBL2019/Models/ModeloNiscoutFBL.Context.cs
+++ NiscoutFBL2019/Models/ModeloNiscoutFBL.Context.cs
@@ -51,6 +51,6 @@ namespace NiscoutFBL2019.Models
         public virtual DbSet<Patrocinador> Patrocinadores { get; set; }
         public virtual DbSet<Personal_Admon> Personal_Admon { get; set; }
         public virtual DbSet<Tutoria> Tutorias { get; set; }
-
+        public virtual DbSet<Membresia_Juvenil> Membresia_Juvenil { get; set; }
     }
 }
