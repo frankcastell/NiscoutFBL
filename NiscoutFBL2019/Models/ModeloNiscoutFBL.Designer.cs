88b9141f59052742567de21f0155ce036e34922f
Brayan Obando Zamora <brayanoz@outlook.com>
2020-01-12 01:45:04 -0600

    ok cambios

--- NiscoutFBL2019/Models/ModeloNiscoutFBL.Designer.cs
+++ NiscoutFBL2019/Models/ModeloNiscoutFBL.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Ing. Francisco\Source\Repos\NiscoutFBL\NiscoutFBL2019\Models\ModeloNiscoutFBL.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\brayan obando\Source\Repos\NiscoutFBL\NiscoutFBL2019\Models\ModeloNiscoutFBL.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
