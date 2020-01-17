debcbc13f9d229ca91663451f01445f8010eb9fb
Ing. Francisco <Ing. Francisco@DIOS>
2020-01-17 11:47:32 -0600

    Suo, Grupos para guardar archivos, falta como visualizar imagen si lee pdf.

--- NiscoutFBL2019/Controllers/Membresia_AdultoController.cs
+++ NiscoutFBL2019/Controllers/Membresia_AdultoController.cs
@@ -77,8 +77,6 @@ namespace NiscoutFBL2019.Controllers
         // PENDIENTE LA CORRECIÓN CON EL MÁSTER RECIBIR PARÁMETRO
         public Byte[] tobyte(HttpPostedFileBase image1)
         {
-           
-
             byte[] buffer;
             using (Stream stream = image1.InputStream)
             {
@@ -92,59 +90,6 @@ namespace NiscoutFBL2019.Controllers
                 return buffer;
         }
        
-        public Byte[] ruta2(HttpPostedFileBase image2)
-        {             
-            Membresia_Adulto membresia_Adulto = new Membresia_Adulto();
-            byte[] imagenData2 = null;
-            if (image2 != null && image2.ContentLength > 0)
-            {               
-                using (var img2 = new BinaryReader(image2.InputStream))
-                {
-                    imagenData2 = img2.ReadBytes(image2.ContentLength);
-                }              
-            }
-            return imagenData2;
-        }
-        public Byte[] ruta3(HttpPostedFileBase image3)
-        {
-            Membresia_Adulto membresia_Adulto = new Membresia_Adulto();
-            byte[] imagenData3 = null;
-            if (image3 != null && image3.ContentLength > 0)
-            {
-                
-                using (var img3 = new BinaryReader(image3.InputStream))
-                {
-                    imagenData3 = img3.ReadBytes(image3.ContentLength);
-                }               
-            }
-            return imagenData3;
-        }
-        public Byte[] ruta4(HttpPostedFileBase image4)
-        {
-            Membresia_Adulto membresia_Adulto = new Membresia_Adulto();
-            byte[] imagenData4 = null;
-            if (image4 != null && image4.ContentLength > 0)
-            {
-                using (var img4 = new BinaryReader(image4.InputStream))
-                {
-                    imagenData4 = img4.ReadBytes(image4.ContentLength);
-                }
-            }
-            return imagenData4;
-        }
-        public Byte[] ruta5(HttpPostedFileBase image5)
-        {
-            Membresia_Adulto membresia_Adulto = new Membresia_Adulto();
-            byte[] imagenData5 = null;
-            if (image5 != null && image5.ContentLength > 0)
-            {
-                using (var img5 = new BinaryReader(image5.InputStream))
-                {
-                    imagenData5 = img5.ReadBytes(image5.ContentLength);
-                }
-            }
-            return imagenData5;
-        }
         // Nueva vista ....................................Inicio
         [AllowAnonymous]
         public ActionResult MembreAdulto(int idAdulto)
@@ -211,10 +156,10 @@ namespace NiscoutFBL2019.Controllers
             if (ModelState.IsValid)
             {
                 membresia_Adulto.Carta_Compromiso = tobyte(image1);
-                membresia_Adulto.Carta_Intencion = ruta2(image2);
-                membresia_Adulto.Record_Policia = ruta3(image3);
-                membresia_Adulto.Carta_Ref_Personal = ruta4(image4);
-                membresia_Adulto.Certifi_Salvo_Peligro = ruta5(image5);
+                membresia_Adulto.Carta_Intencion = tobyte(image2);
+                membresia_Adulto.Record_Policia = tobyte(image3);
+                membresia_Adulto.Carta_Ref_Personal = tobyte(image4);
+                membresia_Adulto.Certifi_Salvo_Peligro = tobyte(image5);
                 db.Entry(membresia_Adulto).State = System.Data.Entity.EntityState.Modified;                
                 db.SaveChanges();
                 return RedirectToAction("Index");
