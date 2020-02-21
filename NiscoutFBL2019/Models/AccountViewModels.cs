28536d5003f8bb08dd4ec8f711084399d435c9e7
Jorge <Jorge@DESKTOP-IJ11RG8>
2020-02-21 17:38:27 -0600

    Usuario:Eliminar,Editar listo

--- NiscoutFBL2019/Models/AccountViewModels.cs
+++ NiscoutFBL2019/Models/AccountViewModels.cs
@@ -118,18 +118,8 @@ namespace NiscoutFBL2019.Models
         [Required]
         [EmailAddress]
         [Display(Name = "Correo electrónico")]
-        public string Email { get; set; }
+        public string Email { get; set; }        
 
-        [Required]
-        [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Contraseña")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirmar contraseña")]
-        [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
-        public string ConfirmPassword { get; set; }
         [Required]
         [Display(Name = "Rol de usuario")]
         public string[] Roles { get; set; }
